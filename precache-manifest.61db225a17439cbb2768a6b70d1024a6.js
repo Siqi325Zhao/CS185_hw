8628153a2904d2e3ee788b118a74a4bdf1503b93
MagZhao <siqi00@umail.ucsb.edu>
2020-04-26 22:08:37 -0700

    Updates

---
+++ precache-manifest.61db225a17439cbb2768a6b70d1024a6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e9978d6162daf3fe73fa54dc4e58e09",
+    "url": "/CS185_hw/index.html"
+  },
+  {
+    "revision": "040b7d3110a684c3fce7",
+    "url": "/CS185_hw/static/css/main.5aabd7d6.chunk.css"
+  },
+  {
+    "revision": "ec9991bc75cf71604c55",
+    "url": "/CS185_hw/static/js/2.bfaea79c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/CS185_hw/static/js/2.bfaea79c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "040b7d3110a684c3fce7",
+    "url": "/CS185_hw/static/js/main.1cdc5111.chunk.js"
+  },
+  {
+    "revision": "8b43da9e69925440fb36",
+    "url": "/CS185_hw/static/js/runtime-main.4a985614.js"
+  },
+  {
+    "revision": "a7e02074ee947797ed29b83e8b6379f1",
+    "url": "/CS185_hw/static/media/dali.a7e02074.jpg"
+  },
+  {
+    "revision": "797919b231d0c647197523c8a4dc2766",
+    "url": "/CS185_hw/static/media/inner mongolia.797919b2.JPG"
+  },
+  {
+    "revision": "b289b90ca478019908c4dffbc49e2c52",
+    "url": "/CS185_hw/static/media/inner mongolia2.b289b90c.JPG"
+  },
+  {
+    "revision": "8941c5f010ed9164e4d7d9e9517ac73e",
+    "url": "/CS185_hw/static/media/luoyang.8941c5f0.JPG"
+  },
+  {
+    "revision": "e211a26e69a2614f7cdce0353bd15772",
+    "url": "/CS185_hw/static/media/malibu.e211a26e.jpg"
+  },
+  {
+    "revision": "9144b758dfd4947b2a6c2ffcc7a05dec",
+    "url": "/CS185_hw/static/media/me.9144b758.jpg"
+  },
+  {
+    "revision": "16be4206970ffb07f8b4230dc731e369",
+    "url": "/CS185_hw/static/media/netease.16be4206.png"
+  },
+  {
+    "revision": "23f2085eea938fc54861db808cdf2bb6",
+    "url": "/CS185_hw/static/media/san francisco.23f2085e.jpg"
+  },
+  {
+    "revision": "3892e3e2d64684c1cfa02fc15e1fdb70",
+    "url": "/CS185_hw/static/media/santa barbara.3892e3e2.jpg"
+  },
+  {
+    "revision": "8d47da90da11badb1c92d7083432e34a",
+    "url": "/CS185_hw/static/media/taobao.8d47da90.jpeg"
+  },
+  {
+    "revision": "8e0f0a9a6558fb00895ac199a2d494c0",
+    "url": "/CS185_hw/static/media/wechat.8e0f0a9a.png"
+  },
+  {
+    "revision": "aa7b2efbdca93af459b4e4fbe8e1c439",
+    "url": "/CS185_hw/static/media/wuhan.aa7b2efb.jpg"
+  },
+  {
+    "revision": "8522159d862b7c9039c2826fe141134a",
+    "url": "/CS185_hw/static/media/youtube.8522159d.png"
+  },
+  {
+    "revision": "bf7ac5a587f8bbec467cb4b59e9f133d",
+    "url": "/CS185_hw/static/media/zhengzhou.bf7ac5a5.jpg"
+  }
+]);
