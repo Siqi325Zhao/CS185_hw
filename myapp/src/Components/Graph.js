38ec51909def05308b7d42ce9041033d2ed63779
MagZhao <siqi00@umail.ucsb.edu>
2020-06-07 18:49:06 -0700

    hw8 with bug

--- myapp/src/Components/Graph.js
+++ myapp/src/Components/Graph.js
@@ -5,6 +5,108 @@ import config from '../config';
 const firebase = require('firebase');
 var d3 = require("d3");
 
+const data={
+		"nodes": [
+		  {
+			"id": 1,
+			"name": "Andy",
+			"gender": "male"
+		  },
+		  {
+			"id": 2,
+			"name": "Betty",
+			"gender": "female"
+		  },
+		  {
+			"id": 3,
+			"name": "Cate",
+			"gender": "female"
+		  },
+		  {
+			"id": 4,
+			"name": "Dave",
+			"gender": "male"
+		  },
+		  {
+			"id": 5,
+			"name": "Ellen",
+			"gender": "female"
+		  },
+		  {
+			"id": 6,
+			"name": "Fiona",
+			"gender": "female"
+		  },
+		  {
+			"id": 7,
+			"name": "Garry",
+			"gender": "male"
+		  },
+		  {
+			"id": 8,
+			"name": "Holly",
+			"gender": "female"
+		  },
+		  {
+			"id": 9,
+			"name": "Iris",
+			"gender": "female"
+		  },
+		  {
+			"id": 10,
+			"name": "Jane",
+			"gender": "female"
+		  }
+		],
+		"links": [
+		  {
+			"source": 1,
+			"target": 2
+		  },
+		  {
+			"source": 1,
+			"target": 5
+		  },
+		  {
+			"source": 1,
+			"target": 6
+		  },
+	  
+		  {
+			"source": 2,
+			"target": 3
+		  },
+		  {
+			"source": 2,
+			"target": 7
+		  }
+		,
+	  
+		  {
+			"source": 3,
+			"target": 4
+		  },
+		  {
+			"source": 8,
+			"target": 3
+		  }
+		,
+		  {
+			"source": 4,
+			"target": 5
+		  }
+		,
+	  
+		  {
+			"source": 4,
+			"target": 9
+		  },
+		  {
+			"source": 5,
+			"target": 10
+		  }
+		]
+}
 
 export class Graph extends Component {
 	constructor() {
@@ -203,9 +305,8 @@ export class Graph extends Component {
 	    					value: 3
 	    				})
 	    			}
-	    		});
-
-
+				});
+				
 	    		this.setState(prevState => ({
 	      			movies: {
 	      				...prevState.movies,
@@ -214,7 +315,7 @@ export class Graph extends Component {
 	     		}));
 	    	});
 	    const elem = document.getElementById("mysvg");
-    	elem.appendChild(this.chart(nodes, links));
+    	elem.appendChild(this.chart(data.nodes, data.links));
     	});
 	}
 
